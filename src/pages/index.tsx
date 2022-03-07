294efdf0a114fbfefc91174c606aafaaad28aed8
Edvaldo Afonso <eadafonso@gmail.com>
2022-03-07 14:25:38 +0100

    feat: create home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,44 @@
+/* eslint-disable @next/next/no-img-element */
 import { NextPage } from "next";
+import Head from "next/head";
+
+import styles from "../styles/heme.module.scss";
 
 const Home: NextPage = () => {
   return (
     <>
-      <h1>Eadafonso blog</h1>
+      <Head>
+        <title>Eadafonso | Home</title>
+      </Head>
+
+      <main className={styles.contentContainer}>
+        <div>
+          <section className={styles.hero}>
+            <span>👏 Hey, welcome</span>
+
+            <h1>
+              News about the
+              <br />
+              <span>Front-End </span>world
+            </h1>
+
+            <p>
+              Get acess to all the publications <br />
+              <span>Sign to newslaters</span>
+            </p>
+
+            {/* <input placeholder="Digite o seu email" /> */}
+          </section>
+
+          <img src="/image/avatar.svg" alt="the girl cod" />
+        </div>
+
+        <span>EAD</span>
+      </main>
+
+      {/* <main className={styles.cardContainer}>
+        <h2>All Post</h2>
+      </main> */}
     </>
   );
 };
