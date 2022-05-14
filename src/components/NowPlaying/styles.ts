d40f2f9bc01eedcbaf858968a9484561c66a1353
Edvaldo Afonso <eadafonso@gmail.com>
2022-05-14 22:44:44 +0100

    feat: create Now play components

---
+++ src/components/NowPlaying/styles.ts
@@ -0,0 +1,17 @@
+import { styled } from "@stitches/react";
+
+export const PlayingContainer = styled("div", {
+  marginTop: "2rem",
+  marginBottom: "1.5rem",
+
+  width: "100%",
+
+  div: {
+    display: "flex",
+    alignItems: "center",
+
+    svg: {
+      marginRight: 20,
+    },
+  },
+});
