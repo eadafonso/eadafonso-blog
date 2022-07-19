12c8bf9f405c287c7b2bda0c5bd51a6e0361e433
Edvaldo Afonso <eadafonso@gmail.com>
2022-07-19 12:43:27 +0100

    refactor: change lib

--- src/lib/spotify.ts
+++ src/lib/spotify.ts
@@ -1,6 +1,3 @@
-//import fetch from 'isomorphic-unfetch';
-import querystring from "querystring";
-
 const client_id = process.env.SPOTIFY_CLIENT_ID;
 const client_secret = process.env.SPOTIFY_CLIENT_SECRET;
 const refresh_token = process.env.SPOTIFY_REFRESH_TOKEN;
